843fe09db476249345d72a95cf251228b3748113
Ichiroki <fahrezirizqiawan12649@gmail.com>
2023-07-03 15:34:13 +0700

    Merubah sistem carousel menggunakan splideJS dan merubah tampilan section 5

---
+++ js/splide.js
@@ -0,0 +1,17 @@
+// Splide
+import Splide from '@splidejs/splide';
+
+// Default theme
+// import '@splidejs/splide/css';
+
+
+// or other themes
+// import '@splidejs/splide/css/skyblue';
+// import '@splidejs/splide/css/sea-green';
+
+
+// or only core styles
+import '@splidejs/splide/css/core';
+        
+
+new Splide('#image-carousel').mount();
